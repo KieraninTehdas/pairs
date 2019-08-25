f447aa1fb73f1659af1e538ee40b5ea67fc880f0
Kieran Porter <k.porter.phys@gmail.com>
2019-08-25 16:25:03 +0100

    Start with basic spa example

---
+++ src/home.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+ 
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Home Page</h2>
+        <p>Choose a deck to play:</p>
+
+        <li>
+            Insert Deck Name Here
+        </li>
+        <li>
+            Insert Deck Name Here
+        </li>
+      </div>
+    );
+  }
+}
+ 
+export default Home;
