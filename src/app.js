2c9ac337be69756086fdfabb1cb4de5870d957b1
Kieran Porter <k.porter.phys@gmail.com>
2019-08-29 21:15:24 +0100

    Move routing into index.js, reduce number of scattered files

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import DeckList, { getDeckNames } from "./deck-service";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Home Page</h2>
+        <p>Choose a deck to play:</p>
+        <div>
+          <DeckList deckNames={getDeckNames()}>
+          </DeckList>
+        </div>
+      </div>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Main from "./main";
+import {
+  Route,
+  NavLink,
+  HashRouter
+} from "react-router-dom";
+import App from "./app";
+import DeckEditor from "./deck-editor";
+import Game from "./game";
+
 import "./index.css";
 
-ReactDOM.render(
-  <Main />,
-  document.getElementById("root")
-);
+const routing = (
+  <HashRouter>
+    <div>
+      <h1>Simple SPA</h1>
+      <ul className="header">
+        <li><NavLink exact to="/">Home</NavLink></li>
+        <li><NavLink to="/deck-editor">DeckEditor</NavLink></li>
+      </ul>
+      <div className="content">
+        <Route exact path="/" component={App} />
+        <Route path="/deck-editor" component={DeckEditor} />
+      </div>
+    </div>
+
+    <Route path="play/:deckName" component={Game}></Route>
+
+  </HashRouter>
+)
+
+ReactDOM.render(routing, document.getElementById("root"));
